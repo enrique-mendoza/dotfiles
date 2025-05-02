aac875532cde4bf3d9a4954b143c22a59c85a8d4
Enrique Mendoza <luis.e.1998@hotmail.com>
2025-05-02 09:57:44 -0600

    feat(zsh): git aliases

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -8,6 +8,30 @@ alias l="eza -F --icons --color=always --group-directories-first"
 alias l.="eza -a | egrep '^\.'"
 alias tree="eza --icons --color=always --tree --level=2 --long"
 
+# Git
+alias g="git"
+alias ga="git add"
+alias gap="ga --patch"
+alias gb="git branch"
+alias gba="gb --all"
+alias gc="git commit"
+alias gca="gc --amend --no-edit"
+alias gce="gc --amend"
+alias gco="git checkout"
+alias gcl="git clone --recursive"
+alias gd="git diff --output-indicator-new=' ' --output-indicator-old=' '"
+alias gds="gd --staged"
+alias gi="git init"
+alias gl="git log --graph --all --pretty=format:'%C(magenta)%h %C(white) %an  %ar%C(blue)  %D%n%s%n'"
+alias gm="git merge"
+alias gn="git checkout -b" # new branch
+alias gp="git push"
+alias gr="git reset"
+alias gs="git status --short"
+alias gu="git pull"
+
+gcm() { git commit --message "$*" }
+
 # Confirm before overwriting something
 alias cp="cp -i"
 alias mv="mv -i"
