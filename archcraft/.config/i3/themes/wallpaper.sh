e5650b4a6f638522007be32e00b5f7fadfff058b
Enrique Mendoza <luis.e.1998@hotmail.com>
2025-05-14 13:10:15 -0600

    update: wallpaper

--- archcraft/.config/i3/themes/wallpaper.sh
+++ archcraft/.config/i3/themes/wallpaper.sh
@@ -4,7 +4,7 @@
 ##
 ## Apply wallpaper on i3 startup
 
-WALLPAPER='/home/kike/.config/i3/themes/forest/wallpapers/24.jpeg'
+WALLPAPER='/home/kike/.config/i3/themes/forest/wallpapers/01.png'
 
 ## For single monitor
 #hsetroot -root -cover "$WALLPAPER"
